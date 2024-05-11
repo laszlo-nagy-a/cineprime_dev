0c6677a36092eda1b5e02d8991c095e2b43cb4db
Laszlo Nagy <laszlo.n620@gmail.com>
2024-05-11 10:31:34 +0200

    Java spring starter hozzáadva

---
+++ src/main/java/com/homeproject/cineprime/CineprimeApplication.java
@@ -0,0 +1,12 @@
+package com.homeproject.cineprime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineprimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineprimeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/homeproject/cineprime/CineprimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeproject.cineprime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineprimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
